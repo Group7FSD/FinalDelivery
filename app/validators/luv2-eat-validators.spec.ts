c9bf73ac37b54dd6f5f4bbbd2b73b17de87905cf
Vashu-Garg <59685871+Vashu-Garg@users.noreply.github.com>
2021-05-17 09:40:47 +0530

    Delete app directory

--- app/validators/luv2-eat-validators.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Luv2EatValidators } from './luv2-eat-validators';
-
-describe('Luv2EatValidators', () => {
-  it('should create an instance', () => {
-    expect(new Luv2EatValidators()).toBeTruthy();
-  });
-});
